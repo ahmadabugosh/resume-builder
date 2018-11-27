935d47d3d9c7120724cf04a96fe28cfc95db4bab
Ahmad Abugosh <ahmadabugosh@gmail.com>
2018-11-27 21:59:47 +0400

    Forked project

---
+++ app/client/src/common/components/index.js
@@ -0,0 +1,13 @@
+/**
+ * @flow
+ */
+
+import Divider from './Divider'
+import Icon from './Icon'
+import Button from './Button'
+import RoundButton from './RoundButton'
+import ScrollToTop from './ScrollToTop'
+import Loader, { Bars } from './Loader'
+import Logo from './Logo'
+
+export { Button, RoundButton, Divider, Icon, ScrollToTop, Loader, Logo, Bars }
---
+++ readme.md
@@ -0,0 +1,7 @@
+![resumake-logo](https://i.imgur.com/Y4JmhrV.png)
+
+# resumake
+> A website for automatically generating elegant LaTeX resumes without the need to write any TeX code yourself.
+
+## License
+MIT ©
